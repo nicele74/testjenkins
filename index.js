e0727e6858c7bc4bacee98f420ba6ad210df4334
nicele74 <68985388+nicele74@users.noreply.github.com>
2020-07-31 15:28:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like u27-pm2');
+    res.send('I like u27-pm2-dontkill');
 });
 
 app.get('/start', function(req, res){
